675b453ce77fc2a8dbd27b3ec822096c0a1dd1c9
matthewn4444 <matthewn4444@61ae86f6-b183-4c0e-610b-53ec448a0a34>
2011-03-12 18:22:10 +0000

    work in progress....
    git-svn-id: https://jobmine-plus.googlecode.com/svn/trunk@121 61ae86f6-b183-4c0e-610b-53ec448a0a34

--- wrap.js
+++ wrap.js
@@ -17,7 +17,8 @@
           &UW_CO_WT_SESSION= <--- copy from cookies, this will be set from job search anomysly
           
      shortlist & work has this but not applications page
-          https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?Menu=UW_CO_STUDENTS&Action=U&UW_CO_JOB_ID=###### <---opens the page dependent on the number
+          https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?Menu=UW_CO_STUDENTS&Component=UW_CO_JOBDTLS&UW_CO_JOB_ID=######          <---opens the page dependent on the number
+          
 */
 
 
@@ -89,34 +90,107 @@ var APPLICATION_PAGE = "https://jobmine.ccol.uwaterloo.ca/servlets/iclientservle
 /*
  *   Redirects
  */
-function focusPass(){document.getElementById("userid").removeEventListener("focus",focusPass,false);document.getElementById('pwd').focus();}
-if(window.location.href.indexOf("jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/ES/") != -1){window.location.href = "https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?cmd=login";return;}
+function focusPass(){
+     document.getElementById("userid").removeEventListener("focus",focusPass,false);
+     document.getElementById('pwd').focus();
+}
+//Redirect all ES's to SS
+if(window.location.href.indexOf("jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/ES/") != -1){
+     window.location.href = "https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?cmd=login";
+     return;
+}
+//If we are on the normal login page
 else if(document.getElementById("search")){
+
      //input checkbox and add an eventlistener
      var newNode = document.createElement("div");
      newNode.style.left = "212px";
      newNode.style.position = "relative";
-     newNode.innerHTML = "<input id='autoCheckID' type='checkbox' title='Click to auto input user ID next time. Cannot save password for security reasons.'/><span style='font-size:11px;'>Save User ID<span>";     
+     newNode.innerHTML = "<input id='rememberUser' type='checkbox'/><span style='font-size:11px;'>Remember User<br/> <span style='color:red;'>(DO NOT CHECK ON PUBLIC PC)</span></span>";     
      document.getElementById("pwd").parentNode.parentNode.parentNode.insertBefore(newNode,document.getElementById("pwd").parentNode.parentNode.nextSibling.nextSibling);
-     document.getElementById("autoCheckID").addEventListener("click",function(){writeCookie('LASTUSER', this.checked ? "1|"+ getCookieValue('LASTUSER').split("|")[1] : 0);},false);
+     document.getElementById("rememberUser").addEventListener("click",function(){writeCookie('REMEMBERUSER', this.checked ? 1 : 0);},false);
      
-     var user = getCookieValue('LASTUSER');
-     if(user != "NaN" && user != -1 && user[0] != 0 && document.getElementById("userid")){
-          document.getElementById("userid").value = user.substr(2);
-          document.getElementById("userid").addEventListener("focus",focusPass,false);
-          document.getElementById("autoCheckID").checked = "checked";
+     //If the user wants to save passwords
+     var user = getCookieValue('REMEMBERUSER');
+     if(user == 1 && document.getElementById("userid")){
+          document.getElementById("login").setAttribute("autocomplete","on");
+          document.getElementById("rememberUser").checked = "checked";
      }
+     else
+     {
+          document.getElementById("userid").setAttribute("autocomplete","off");
+          document.getElementById("pwd").setAttribute("autocomplete","off");
+     }
+   
      return;
 }
+/*
+ *   Searches for the ID for a job description when clicking the jobs in applications list
+ */
+else if(IS_IN_IFRAME && document.title == "Job Details" )
+{
+     var foundJobID = 0;                                                   //Holds the id if found on the page, if not it is 0
+
+     //We hit the search page and we want to hurry and press the button
+     if(window.location.href.indexOf("https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?Menu=UW_CO_STUDENTS&Component=UW_CO_JOBDTLS&Page=UW_CO_STU_JOBDTLS&UW_CO_JOB_TITLE=") != -1){
+          //Firefox or Chrome?
+          window.location = UNSAFEWINDOWSUPPORT ? "javascript:submitAction_main(document,'main', '#ICSearch')" : "submitAction_main(document.main, '#ICSearch')";
+          return;
+          //After this it will run one of the two if else statements below
+     }
+     //1. Check if the page is a search page (lookup for jobs)
+     else if(document.body.className.indexOf("PSSRCHPAGE") != -1){
+          //Init Variables
+          var nextNode = {};                                               //Cycles through later on and holds the current object     
+          var index =  0;                                                     //Cycling index
+          var term = getCurrentTerm();                                 //Gets the term
+          var nodes = document.getElementsByTagName("a");   //Holds all the a-tags on the page, we look in this
+             
+          //Look for a link (a-tag) for the hyperlink of the correct term
+          while (foundJobID == 0 && nodes[++index])
+          {    //If we find the term in the text of an a-tag, we have found it!
+               if(nodes[index].firstChild && term == nodes[index].firstChild.nodeValue){
+                    foundJobID = nodes[index-1].firstChild.nodeValue;    //We set the previous a-tag's value which is the id and not the term                         
+               }
+          }
+          //We found it, if we haven't then it is Jobmine's fault, we now can make a new tab
+          if(foundJobID != 0){
+               window.open("https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?Menu=UW_CO_STUDENTS&Component=UW_CO_JOBDTLS&UW_CO_JOB_ID=" + foundJobID);
+          }else{
+               alert("CANNOT FIND ID");
+               return;
+          }
+     }
+     //2. This is when the search for the lookup jobs loads a description instead of a list of indexes
+     else if(window.location.href.indexOf("?ICType=Panel&Menu=UW_CO_STUDENTS&Market=GBL&PanelGroupName=UW_CO_JOBDTLS") != -1)
+     {
+          //This page is the job details page with a specific url, we grab the job ID and run
+          foundJobID = document.getElementsByTagName("div")[6].firstChild.nodeValue;             //6 is the location of the jobID, jobmine loads this as a template, it will hopefully never change
+          
+          //Make sure it is a number
+          if(isNumeric( foundJobID )){
+               window.open("https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?Menu=UW_CO_STUDENTS&Component=UW_CO_JOBDTLS&UW_CO_JOB_ID=" + foundJobID);
+          }else{
+               alert("GRABBED THE WRONG THING: you got "+ foundJobID);
+               return;
+          }
+     }
+     
+     //Write the job id in a cookie
+     writeCookie("APP_LAST_ID", foundJobID);
+     
+     //Go to a blank page when finished
+     window.location = "about:blank";
+     return;
+}
+//Right when Jobmine logins in
 else if(window.location.href == 'https://jobmine.ccol.uwaterloo.ca/servlets/iclientservlet/SS/?cmd=start&')
 {
      document.getElementsByTagName("html")[0].innerHTML = "<body>"+white_overlay("<div style='text-align:justify;'>Welcome to Jobmine Plus! No Jobmine Plus is not slower than Jobmine (well only by a bit since it does extra enhancements after), it is about the same timing. If you don't like this because it is slower, then you will miss out on all the great features. By the time you read this, the next page would have loaded.</div>",21,false)+"</body>";	
      document.getElementById('popupWhiteContainer').style.display = "block";
      document.getElementById('popupWhiteContent').style.top = "-130px";
      window.stop();
-     
-     var user = getCookieValue('LASTUSER');
-     if (user[0] != 0 || user != -1){writeCookie("LASTUSER", "1|"+getCookieValue("SignOnDefault").toLowerCase());}
+          
      setTimeout(function(){
           var default_page = getCookieValue('DEFAULT_PAGE');
           switch(default_page){
@@ -148,6 +222,8 @@ else if(window.location.href == 'https://jobmine.ccol.uwaterloo.ca/servlets/icli
      },1);
      return;
 }
+
+
 
 
 
